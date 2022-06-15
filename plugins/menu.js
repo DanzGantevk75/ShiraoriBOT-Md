c302f1af19851ef758542543f857e43454d8830a
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-06-15 17:26:03 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -167,14 +167,14 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
            hydratedButtons: [{
              urlButton: {
                displayText: '💠 Source Code',
-               url: 'https://github.com/ilmanhdyt/ShiraoriBOT-Md'
+               url: 'Chat Owner Aja,Free'
              }
 
            },
              {
              callButton: {
                displayText: 'Nomor Owner',
-               PhoneNumber: '0813-5104-7727'
+               PhoneNumber: '0882-7086-3279'
              }
 
            },
