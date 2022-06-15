a4edc5dfb240207edbb4a9322f44b6cd8cefa650
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-06-15 17:22:42 +0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,7 +1,7 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw 'usernamenya mana om\nContoh ilmanhdyt'
-if (!args[1]) throw 'repo nya mana?\nContoh ShiraoriBOT'
+if (!args[0]) throw 'usernamenya mana om\nContoh Danzz'
+if (!args[1]) throw 'repo nya mana?\nContoh DanzBot'
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
 //ByRizkyAdi
